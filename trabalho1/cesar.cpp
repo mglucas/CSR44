d77d7422e6f3ece7ed307564a9fe3b42e7e30dd0
mglucas <mglucas@debian>
2019-08-24 11:47:44 -0300

    95% cesar - wting tests

--- trabalho1/cesar.cpp
+++ trabalho1/cesar.cpp
@@ -3,27 +3,62 @@
 #include<ctype.h>
 #include<stdlib.h>
 
-int main(int argc,char *argv[]){
+char chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  char c; // encrypt signal
-  int k; // k = chave  
+
+int char_valido(char c){
+  char* ptr = strchr(chars,c);
   
-  if(!strcmp(argv[2],"-k"))
-  k = atoi(argv[3]);
+  if(ptr == NULL) return -1;
+  else
+  return ptr-chars;
+}
 
+
+int main(int argc,char *argv[]){
+  
+  // encrypt signal
+  char c; 
+  // k = chave  
+  int k;
+  // Tamanho do vetor de chars 
+  int tam_chars = sizeof(chars)/sizeof(chars[0]) - 1;
+  // char position returned as int
+  signed int pos;
+ 
+  // key 
+  if(!strcmp(argv[2],"-k")) k = atoi(argv[3]);
+
+  // encrypt
   if(!strcmp(argv[1],"-c")){
-    while((c=getchar())!= '\n'){
-      if(isalpha(c)) printf("%c",c+k);
-      if(isspace(c)) putchar(' ');
+    while((c=getchar())!= EOF){
+      pos = char_valido(c);
+      
+      if(pos == -1){  
+        if(c=='\n') putchar('\n');
+        else
+        if(isspace(c)) putchar(' ');
+      } 
+      else
+      printf("%c",chars[(pos+k)%tam_chars]);
+      
     }
     putchar('\n'); 
   }
-
- else
- if(!strcmp(argv[1],"-d")){
-    while((c=getchar())!= '\n'){
-      if(isalpha(c)) printf("%c",c-k);
-      if(isspace(c)) putchar(' ');
+  // decrypt
+  else
+  if(!strcmp(argv[1],"-d")){
+    while((c=getchar())!= EOF){
+      pos = char_valido(c);
+      
+      if(pos == -1){  
+        if(c=='\n') putchar('\n');
+        else
+        if(isspace(c)) putchar(' ');
+      } 
+      else
+      printf("%c",chars[(pos-k)%tam_chars]);
+      
     }
     putchar('\n'); 
   }
